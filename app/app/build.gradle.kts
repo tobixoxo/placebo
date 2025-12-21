a79c3bca7f107c61aec292b97ec482a1d44395e1
The5thAxiom <samridh.anand.paatni@gmail.com>
2025-12-21 22:19:12 +0530

    created new android project

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.tobaxiom.placebo"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
--- app/app/src/main/java/com/tobaxiom/placebo/Streak.kt
+++ app/app/src/main/java/com/tobaxiom/placebo/Streak.kt
@@ -1,7 +1,26 @@
 package com.tobaxiom.placebo
 
-class Streak {
-    public val name: String
+import java.time.LocalDate
+import java.util.UUID
 
-    Streak()
+class Streak(var name: String){
+    var id: UUID = UUID.randomUUID()
+    var markedDays = mutableListOf<LocalDate>()
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Streak
+
+        return id != other.id
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+
+    fun markForToday() {
+        markedDays.add(LocalDate.now())
+    }
 }
